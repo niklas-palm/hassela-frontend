daaf5b6704932b4815374abd137172fe1ba10299
Niklas Palm <nipalm@amazon.com>
2022-06-30 20:53:41 +0200

    fetch urls from api and get API_URL from gitignored file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { GeistProvider, CssBaseline, Grid, Card } from "@geist-ui/core";
+import { Amplify, Auth } from "aws-amplify";
+import {
+  AmplifyAuthenticator,
+  // AmplifySignOut, // use as component to add signout capabilties.
+  AmplifySignUp,
+  AmplifySignIn,
+} from "@aws-amplify/ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
+
+import ReactHlsPlayer from "react-hls-player";
+
+import { getUrls } from "./utils/requests";
+
+import "./App.scss";
+
+const renderStreams = (streams) => {
+  return streams.map((stream) => {
+    return (
+      <Grid xs={24} md={12} key={stream}>
+        <Card width="100%" height="auto">
+          <ReactHlsPlayer
+            src={stream}
+            autoPlay={true}
+            controls={true}
+            width="100%"
+            height="auto"
+          />
+        </Card>
+      </Grid>
+    );
+  });
+};
+
+Amplify.configure(awsconfig);
+
+const App = () => {
+  const [streams, setStreams] = useState([]);
+  const [authState, setAuthState] = useState();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  useEffect(() => {
+    const getStreams = async () => {
+      // Get stream URLs here
+      let res = await getUrls();
+      setStreams(res.streams);
+    };
+    getStreams();
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+    <GeistProvider>
+      <CssBaseline />
+      <Grid.Container gap={2} justify="center">
+        {streams && renderStreams(streams)}
+      </Grid.Container>
+    </GeistProvider>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        usernameAlias="email"
+        formFields={[{ type: "email" }, { type: "password" }]}
+      />
+      <AmplifySignIn slot="sign-in" usernameAlias="email" />
+    </AmplifyAuthenticator>
   );
-}
+};
 
 export default App;
---
+++ src/utils/requests.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { Auth } from "aws-amplify";
+import endpoint from "../endpoint";
+
+const API_URL = endpoint.API_URL;
+
+const getJwtToken = async () => {
+  return (await Auth.currentSession()).getIdToken().getJwtToken();
+};
+
+export const getUrls = async () => {
+  const jwtToken = await getJwtToken();
+
+  const URL = API_URL;
+
+  const headers = {
+    Authorization: jwtToken,
+  };
+
+  console.log(jwtToken);
+
+  const res = await axios.get(URL, { headers });
+
+  return res.data;
+};
