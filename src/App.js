2eb0a580db37822a7d99c01f8a60fb61f0ae439b
Niklas Palm <nipalm@amazon.com>
2022-07-01 11:40:34 +0200

    Add loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { GeistProvider, CssBaseline, Grid, Card } from "@geist-ui/core";
+import {
+  GeistProvider,
+  CssBaseline,
+  Grid,
+  Card,
+  Loading,
+} from "@geist-ui/core";
 import { Amplify } from "aws-amplify";
 import {
   AmplifyAuthenticator,
@@ -17,38 +23,10 @@ import { getUrls } from "./utils/requests";
 
 import "./App.scss";
 
-const renderStreams = (streams, loading) => {
-  if (!streams && !loading) {
-    return (
-      <Grid xs={24} md={12}>
-        <Card width="100%" height="auto">
-          <p>No stream currently available. Try refreshing in a bit</p>
-        </Card>
-      </Grid>
-    );
-  }
-  return streams.map((stream) => {
-    return (
-      <Grid xs={24} md={12} key={stream}>
-        <Card width="100%" height="auto">
-          <ReactHlsPlayer
-            src={stream}
-            autoPlay={true}
-            controls={true}
-            width="100%"
-            height="auto"
-          />
-        </Card>
-      </Grid>
-    );
-  });
-};
-
 Amplify.configure(awsconfig);
 
 const App = () => {
   const [streams, setStreams] = useState();
-  const [loading, setLoading] = useState(true);
   const [authState, setAuthState] = useState();
   const [user, setUser] = useState();
 
@@ -56,7 +34,6 @@ const App = () => {
     onAuthUIStateChange((nextAuthState, authData) => {
       setAuthState(nextAuthState);
       setUser(authData);
-      setLoading(false);
     });
   }, []);
 
@@ -70,11 +47,50 @@ const App = () => {
     getStreams();
   }, []);
 
+  const renderStreams = () => {
+    // Not laoding, and we recived an HLS stream from backend
+    if (streams && streams.length > 0) {
+      return streams.map((stream) => {
+        return (
+          <Grid xs={24} md={12} key={stream}>
+            <Card width="100%" height="auto">
+              <ReactHlsPlayer
+                src={stream}
+                autoPlay={true}
+                controls={true}
+                width="100%"
+                height="auto"
+              />
+            </Card>
+          </Grid>
+        );
+      });
+    }
+
+    // Not loading anymore, but received empty result from backend
+    if (streams && streams.length === 0) {
+      return (
+        <Grid xs={24} md={12}>
+          <Card width="100%" height="auto">
+            <p>No stream currently available. Try refreshing in a bit</p>
+          </Card>
+        </Grid>
+      );
+    }
+
+    // Still waiting
+    return (
+      <Grid xs={24} md={12}>
+        <Loading type="success" height="100px" />
+      </Grid>
+    );
+  };
+
   return authState === AuthState.SignedIn && user ? (
     <GeistProvider>
       <CssBaseline />
       <Grid.Container gap={2} justify="center">
-        {!loading && renderStreams(streams, loading)}
+        {renderStreams()}
       </Grid.Container>
     </GeistProvider>
   ) : (
